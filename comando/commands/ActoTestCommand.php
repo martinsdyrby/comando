24680e74be5504d16ee1498b12d169dbb68da9b4
martin@dyrby.org <martin@dyrby.org@ec6bec29-4be5-bc2e-4ce3-886ee02ba713>
2012-08-10 08:27:32 +0000

    commands folder deleted

--- comando/commands/ActoTestCommand.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: martindyrby
- * Date: Mar 16, 2012
- * Time: 10:13:10 PM
- * To change this template use File | Settings | File Templates.
- */
--- comando/commands/ConstantsTestCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: martindyrby
- * Date: Mar 16, 2012
- * Time: 10:13:40 PM
- * To change this template use File | Settings | File Templates.
- */
- 
