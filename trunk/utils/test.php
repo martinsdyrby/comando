d6edd69343bd2d6ee4219e8374e8f5d69b8edb21
martin@dyrby.org <martin@dyrby.org@ec6bec29-4be5-bc2e-4ce3-886ee02ba713>
2012-09-28 09:54:49 +0000

    resolve resources properly

--- trunk/utils/test.php
+++ trunk/utils/test.php
@@ -120,20 +120,18 @@ EOT;
 
         $filepath = $this->resolveFilepath('/');
 
-
-//        $result->addToHeader('<script type="text/javascript" src="http://libs.molamil.com/zumo-0.1.min.js"></script>');
-        $result->addToHeader('<script type="text/javascript" src="'.$filepath.'zumo-0.1.js"></script>');
+        $result->addToHeader('<script type="text/javascript" src="'.$filepath.'resources/zumo-0.1.js"></script>');
         $result->addToHeader('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>');
-        $result->addToHeader('<script type="text/javascript" src="'.$filepath.'comando.js.php?service='.$_REQUEST['service'].'"></script>');
+        $result->addToHeader('<script type="text/javascript" src="'.$filepath.'resources/comando.js.php?service='.$_REQUEST['service'].'"></script>');
         $result->addToHeader('<link href="http://fonts.googleapis.com/css?family=Andada" rel="stylesheet" type="text/css" />');
-        $result->addToHeader('<link href="'.$filepath.'style.css" rel="stylesheet" type="text/css" />');
+        $result->addToHeader('<link href="'.$filepath.'resources/style.css" rel="stylesheet" type="text/css" />');
 
 
         $site = <<<EOT
         <div id="site">
 
             <div id="header">
-                <img id="logo" src="{$filepath}molalogo.png" alt="MOLAMIL" />
+                <img id="logo" src="{$filepath}resources/molalogo.png" alt="MOLAMIL" />
                 <ul id="menu">
                 </ul>
             </div>
@@ -192,14 +190,42 @@ EOT;
 
     private function resolveFilepath($separator) {
         $php_self = substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')+1);
-        $file = substr(__FILE__, strpos(__FILE__,$php_self)+strlen($php_self));
-        $lastSlash = strrpos($file, '/');
-        if($lastSlash !== false) {
-            return substr($file, 0, $lastSlash).$separator;
-        }
 
-        return $file;
+        $file = substr(__FILE__, 0, strrpos(__FILE__, '/')+1);
+        $dir_parts = explode('/',getcwd());
+        $file_parts = explode('/',$file);
+        $diff = $this->diff($dir_parts,$file_parts);
+        $last_part = array_pop($diff);
+        $out = count($last_part['d']);
+
+        $prefix = "";
+        for($i = 0; $i < $out; $i++) {
+            $prefix .= '../';
+        }
+        return $prefix.implode($separator, $last_part['i']);
     }
 
+    private function diff($old, $new){
+        $maxlen = 0;
+    	foreach($old as $oindex => $ovalue){
+    		$nkeys = array_keys($new, $ovalue);
+    		foreach($nkeys as $nindex){
+    			$matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
+    				$matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+    			if($matrix[$oindex][$nindex] > $maxlen){
+    				$maxlen = $matrix[$oindex][$nindex];
+    				$omax = $oindex + 1 - $maxlen;
+    				$nmax = $nindex + 1 - $maxlen;
+    			}
+    		}
+    	}
+
+    	if($maxlen == 0) return array(array('d'=>$old, 'i'=>$new));
+
+    	return array_merge(
+    		$this->diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+    		array_slice($new, $nmax, $maxlen),
+            $this->diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
+    }
 }
 ?>
