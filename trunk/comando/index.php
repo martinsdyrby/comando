dd4d2d789343ebab177c8d5a042b937b7bd96d2e
md@molamil.com <md@molamil.com@ec6bec29-4be5-bc2e-4ce3-886ee02ba713>
2012-12-05 10:59:37 +0000

    release 0.3  - added get error and get location on ComandoResponse  - added isDebug on Comando  - moved header and jsonp logic from index.php  to display method

--- trunk/comando/index.php
+++ trunk/comando/index.php
@@ -1,30 +1,6 @@
 <?php
-    // ************************************
-    // DON'T EDIT
-    // ************************************
     require_once('comando.php');
     $comando = new Comando();
     $comando->init($_REQUEST);
-
-    if(isset($_REQUEST['service'])) {
-        $result = $comando->display($_REQUEST['service']);
-
-        if($result != null) {
-            if($result->hasLocation()) {
-                header('Location: '.$result->location());
-                exit;
-            } else {
-                $response = $result->response();
-                if(isset($_REQUEST['callback'])) {
-                    echo $_REQUEST['callback'].'('.$response.');';
-                } else {
-                    echo $response;
-                }
-            }
-        } else {
-            header('HTTP/1.0 404 Not Found');
-        }
-    } else {
-        header('HTTP/1.0 404 Not Found');
-    }
+    $comando->display();
 ?>
